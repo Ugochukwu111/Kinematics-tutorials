4210c14413c943e57a51db3a1d6a55882ce12741
Pascal <pas111cal3@gmail.com>
2025-02-28 13:37:02 +0100

    made seaction-1 to 100svh, added corse section use js to loop and show courses

--- script.js
+++ script.js
@@ -16,3 +16,57 @@ showSidebarBtn.addEventListener('click', () => {
   //   showSidebarBtn.setAttribute("aria-expanded", true);
   }
 })
+
+
+// this array contains major courses that 100lvl students offers in uniben
+// --that kinematics teaches
+let hundredLVLCourseLIst =[
+  '**100_Course__1**',
+  '**100_Course__2**',
+  '**100_Course__3**',
+  '**100_Course__4**',
+  '**100_Course__5**'
+];
+
+let html100lvlCourseList = [];
+
+// this array contains major courses that 200lvl students offers in uniben
+// --that kinematics teaches
+const twoHundredLVLCourseLIst =[
+  '**200_Course__1**',
+  '**200_Course__2**',
+  '**200_Course__3**',
+  '**200_Course__4**',
+  '**200_Course__5**'
+];
+
+let html200lvlCourseList = [];
+
+//save both lenghts of the array in variable to enhance faster loopiing
+let hundredLVLCourseLIstLength = hundredLVLCourseLIst.length;
+let twoHundredLVLCourseLIstLength = twoHundredLVLCourseLIst.length;
+
+//the element that holds the courses
+let courseContainer = document.querySelector('.course-list-holder');
+console.log(courseContainer)
+
+//reuseable function to loop 200lvl and 100lvl courseList
+function  displayCourseList(courseList, courseListlength, htmllvllist) {
+  htmllvllist.length = 0;// lent is set to 0 to prevent accumulation of old data
+  for (let i = 0; i < courseListlength; i++){
+    let loopedArray = courseList[i];
+     let html = `<p>${loopedArray}</p>`;
+     htmllvllist.push(html);
+       
+  }
+  courseContainer.innerHTML = htmllvllist.join("");
+}
+ function display100lvlCourseList() {
+  displayCourseList(hundredLVLCourseLIst, hundredLVLCourseLIstLength, html100lvlCourseList);
+ }
+ display100lvlCourseList()
+
+ function display200lvlCourseList(){
+  displayCourseList(twoHundredLVLCourseLIst, twoHundredLVLCourseLIstLength, html200lvlCourseList);
+ }
+
